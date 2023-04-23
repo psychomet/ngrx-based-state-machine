5fef2edd12a138f26e7e99385a7dc1042fced959
psychomet <sanatkar.dev@gmail.com>
2023-04-23 09:59:42 +0330

    feat(showcase): init users comp

--- packages/showcase/src/main.ts
+++ packages/showcase/src/main.ts
@@ -5,13 +5,27 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideStore, provideState } from '@ngrx/store';
+import { ActionsSubject, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import {
+  COMPONENT_STATE_FEATURE_KEY,
+  ComponentStateBuilder,
+  ComponentStateFacade,
+  ComponentStateMachine,
+  componentStateReducer,
+  ComponentStateService,
+} from 'ngrx-fsm';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideEffects(),
-    provideStore(),
+    provideStore({
+      [COMPONENT_STATE_FEATURE_KEY]: componentStateReducer,
+    }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    ComponentStateFacade,
+    ComponentStateBuilder,
+    ComponentStateService,
+    { provide: ActionsSubject, useClass: ComponentStateMachine },
   ],
 }).catch((err) => console.error(err));
