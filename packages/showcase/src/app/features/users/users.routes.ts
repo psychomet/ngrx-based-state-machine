728574d87e073306050093550fc9b18547542508
psychomet <sanatkar.dev@gmail.com>
2023-04-23 10:06:29 +0330

    feat(showcase): generate +state for users comp

--- packages/showcase/src/app/features/users/users.routes.ts
+++ packages/showcase/src/app/features/users/users.routes.ts
@@ -1,8 +1,16 @@
 import { Route } from '@angular/router';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
 
 export const usersRoutes: Route[] = [
   {
     path: '',
     loadComponent: () => import('./components').then((c) => c.UsersComponent),
+    providers: [
+      provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+      provideEffects(UsersEffects),
+    ],
   },
 ];
